d790e2ae47350acc55e04dadc69c8e9507e3d3f5
Yahor Pashko <yahor.pashko@customertimes.com>
2022-07-05 01:04:47 +0400

    Change BrowserRouter to Hash router

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@
 // @ts-expect-error
 import { I18nextProvider } from "react-i18next";
 import { QueryClientProvider } from "react-query";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import queryClient from "./lib/api/queryClient";
 import i18n from "./lib/other/i18n";
 import Routes from "./routes";
 
+console.info("update version 1");
 const App = () => {
   return (
     <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes />
-        </BrowserRouter>
+        </HashRouter>
       </QueryClientProvider>
     </I18nextProvider>
   );
