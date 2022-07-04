15dd9cdb23041d64f265af9acc3e4e90019ef5c0
Yahor Pashko <yahor.pashko@customertimes.com>
2022-07-05 00:48:56 +0400

    Updates

---
+++ locales/index.js
@@ -0,0 +1,3 @@
+export { default as common } from "./en/common.json";
+export { default as validation } from "./en/validation.json";
+export { default as intermediary } from "./en/intermediary.json";
